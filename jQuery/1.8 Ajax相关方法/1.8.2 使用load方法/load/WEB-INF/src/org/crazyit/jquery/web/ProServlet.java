2d179f972502947adc2bcd40cb9b08b047a3d857
YangyongOu <2379599060@qq.com>
2018-10-25 18:21:50 +0800

    oyy

--- jQuery/1.8 Ajax相关方法/1.8.2 使用load方法/load/WEB-INF/src/org/crazyit/jquery/web/ProServlet.java
+++ jQuery/1.8 Ajax相关方法/1.8.2 使用load方法/load/WEB-INF/src/org/crazyit/jquery/web/ProServlet.java
@@ -6,16 +6,6 @@ import javax.servlet.annotation.*;
 
 import java.io.*;
 
-/**
- * Description:
- * <br/>网站: <a href="http://www.crazyit.org">疯狂Java联盟</a>
- * <br/>Copyright (C), 2001-2018, Yeeku.H.Lee
- * <br/>This program is protected by copyright laws.
- * <br/>Program Name:
- * <br/>Date:
- * @author Yeeku.H.Lee kongyeeku@163.com
- * @version 1.0
- */
 @WebServlet(urlPatterns="/pro")
 public class ProServlet extends HttpServlet
 {
