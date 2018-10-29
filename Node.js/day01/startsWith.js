6214abec81918ec84b654307204a73a545bb1eeb
YangyongOu <2379599060@qq.com>
2018-10-29 18:58:04 +0800

    oyy

---
+++ Node.js/day01/startsWith.js
@@ -0,0 +1,6 @@
+var str = "To be, or not to be, that is the question.";
+// startsWith(searchString[, position])
+console.log(str.startsWith('To be')); // true
+console.log(str.startsWith('not to be')); // false
+console.log(str.startsWith('not to be', 10)); // true
+
