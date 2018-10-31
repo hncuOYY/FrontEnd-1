6a4cb002ec591f1a10f5ca708de5645733ce04c4
YangyongOu <2379599060@qq.com>
2018-10-31 16:16:39 +0800

    oyy

--- Node/day02/readFileSync.js
+++ Node/day02/readFileSync.js
@@ -1,3 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
+// fs.readFileSync(path[, options])
+let data = fs.readFileSync(path.join(__dirname, 'files/score.txt'), 'utf8');
+console.log(data);
