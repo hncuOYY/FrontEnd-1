b37f18f744819513002552beb46f9e1ca868b807
YangyongOu <2379599060@qq.com>
2018-11-08 16:34:44 +0800

    oyy

--- Node/2.0 中间件/momentTest/momentTest.js
+++ Node/2.0 中间件/momentTest/momentTest.js
@@ -1,7 +1,7 @@
 // 导入moment模块
 const moment = require('moment')
 
-console.log(moment()); // moment("2018-11-08T15:15:02.381")
+console.log(moment()) // moment("2018-11-08T15:15:02.381")
 const dataStr = moment().format('YYYY-MM-DD HH:mm:ss')
 console.log(dataStr) // 2018-11-08 15:15:57
 
